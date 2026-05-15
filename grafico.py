bf19accba90f762ef1c950eea13d6ed1ae8dabc4
01Paulina <paulinalondonodiaz01@gmail.com>
2026-05-14 21:28:47 -0500

    Se realiza La modificación de los graficos y el main

--- grafico.py
+++ grafico.py
@@ -2,19 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df_ciudad = pd.read_csv('data/processed/usuarios_limpios.csv')
+def generar_visualizaciones(df_usuarios, df_cursos):
+    # --- GRÁFICO 1: TOP 5 CIUDADES ---
+    plt.figure(figsize=(10, 6))
+    
+    # Calculamos el top 5
+    top_5_ciudades = df_usuarios['ciudad'].value_counts().head(5)
+    
+    sns.barplot(x=top_5_ciudades.index, y=top_5_ciudades.values, palette='viridis')
+    
+    plt.title('Top 5 Ciudades con Más Usuarios', fontsize=16)
+    plt.xlabel('Ciudad', fontsize=12)
+    plt.ylabel('Cantidad de Usuarios', fontsize=12)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()  # Se abre la primera ventana
 
-orden = df_ciudad['ciudad'].value_counts().index
+    # --- GRÁFICO 2: DIFICULTAD MÁS COMÚN ---
+    plt.figure(figsize=(8, 5))
 
-sns.set_theme(style="whitegrid")
-plt.figure(figsize=(12, 6))
-
-sns.countplot(data=df_ciudad, x='ciudad', color='red', hue='ciudad', legend=False)
-
-plt.title('Distribución de Usuarios por Ciudad', fontsize=16)
-plt.xlabel('Ciudad', fontsize=12)
-plt.ylabel('Cantidad de Usuarios', fontsize=12)
-plt.xticks(rotation=45)
-
-plt.tight_layout()
-plt.show()
+    df_cursos['dificultad'] = df_cursos['dificultad'].str.lower()
+    
+    # Usamos countplot para ver cuál se repite más
+    sns.countplot(data=df_cursos, x='dificultad', palette='magma', order=df_cursos['dificultad'].value_counts().index)
+    
+    plt.title('Dificultad de Cursos Más Común', fontsize=16)
+    plt.xlabel('Nivel de Dificultad', fontsize=12)
+    plt.ylabel('Frecuencia', fontsize=12)
+    plt.tight_layout()
+    plt.show()  # Se abre la segunda ventana al cerrar la anterior
